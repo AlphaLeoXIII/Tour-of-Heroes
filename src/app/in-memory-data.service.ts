e5fff4601e5e411f1e118774fbd32a5a09f7aaf3
AlphaLeoXIII <108304634+AlphaLeoXIII@users.noreply.github.com>
2024-05-22 12:00:57 +0200

    Add files via upload

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 12, name: 'Red-hair Shanks' },
+      { id: 13, name: 'Eustass Captain Kid' },
+      { id: 14, name: 'Trafalgar Law' },
+      { id: 15, name: 'Gold D. Rodger' },
+      { id: 16, name: 'Straw-hat Luffy' },
+      { id: 17, name: 'Monkey D. Dragon' },
+      { id: 18, name: 'Roronoa Zoro' },
+      { id: 19, name: 'Portgas D. Ace' },
+      { id: 20, name: 'Vinsmoke Sanji' }
+    ];
+
+    return {heroes};
+  }
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Red-hair Shanks' },
+  { id: 13, name: 'Eustass Captain Kid' },
+  { id: 14, name: 'Trafalgar Law' },
+  { id: 15, name: 'Gold D. Rodger' },
+  { id: 16, name: 'Straw-hat Luffy' },
+  { id: 17, name: 'Monkey D. Dragon' },
+  { id: 18, name: 'Roronoa Zoro' },
+  { id: 19, name: 'Portgas D. Ace' },
+  { id: 20, name: 'Vinsmoke Sanji' }
+];
+
