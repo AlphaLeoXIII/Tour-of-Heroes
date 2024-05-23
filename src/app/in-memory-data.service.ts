0c2ec33fdac706c7b59dc4defecd0b29610cb1fb
AlphaLeoXIII <108304634+AlphaLeoXIII@users.noreply.github.com>
2024-05-23 08:52:18 +0200

    Update in-memory-data.service.ts

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -32,8 +32,3 @@ export class InMemoryDataService implements InMemoryDbService {
 }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
